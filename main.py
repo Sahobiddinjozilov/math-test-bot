e1cfe71320e11351bcfe437de35104be4f476261
Sahobiddinjozilov <sahobiddinjozilov@gmail.com>
2026-06-18 19:06:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from aiogram import Bot, Dispatcher, executor, types
+
+import requests
+import time
 
 TOKEN = "8841744903:AAGLRpnTj00af2rj1QLDh2vMfKr2AK6GMYQ"
+URL = f"https://api.telegram.org/bot{TOKEN}/"
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+offset = None
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer("Bot ishlayapti ✅")
+while True:
+    res = requests.get(URL + "getUpdates", params={"offset": offset}).json()
 
-if name == "main":
-    executor.start_polling(dp, skip_updates=True)
+    if "result" in res:
+        for update in res["result"]:
+            offset = update["update_id"] + 1
+
+            if "message" in update:
+                chat_id = update["message"]["chat"]["id"]
+                text = update["message"].get("text", "")
+
+                requests.post(URL + "sendMessage", data={
+                    "chat_id": chat_id,
+                    "text": "Bot ishlayapti ✅"
+                })
+
+    time.sleep(1)
