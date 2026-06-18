444ff50ae594236c0bcce9fd502982c25a8ce92f
Sahobiddinjozilov <sahobiddinjozilov@gmail.com>
2026-06-18 18:57:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, executor, types
 
 TOKEN = "8841744903:AAGLRpnTj00af2rj1QLDh2vMfKr2AK6GMYQ"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
-@dp.message()
+@dp.message_handler()
 async def echo(message: types.Message):
     await message.answer("Bot ishlayapti ✅")
 
-async def main():
-    await dp.start_polling(bot)
-
 if name == "main":
-    asyncio.run(main())
+    executor.start_polling(dp, skip_updates=True)
