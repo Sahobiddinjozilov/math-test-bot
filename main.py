0ab1a2c3d55e3cdbf89aa217c0b69c7b4f2b0614
Sahobiddinjozilov <sahobiddinjozilov@gmail.com>
2026-06-18 18:53:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import os
-from aiohttp import web
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+TOKEN = "8841744903:AAGLRpnTj00af2rj1QLDh2vMfKr2AK6GMYQ"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -14,18 +10,8 @@ dp = Dispatcher()
 async def echo(message: types.Message):
     await message.answer("Bot ishlayapti ✅")
 
-async def on_startup(app):
-    await bot.set_webhook(WEBHOOK_URL + WEBHOOK_PATH)
+async def main():
+    await dp.start_polling(bot)
 
-async def on_shutdown(app):
-    await bot.delete_webhook()
-
-app = web.Application()
-
-SimpleRequestHandler(dp, bot).register(app, path=WEBHOOK_PATH)
-setup_application(app, dp, bot=bot)
-
-app.on_startup.append(on_startup)
-app.on_shutdown.append(on_shutdown)
-
-web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if name == "main":
+    asyncio.run(main())
